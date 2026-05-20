e22c697f0e8d82a7576012bff589dec4bec8fed3
Leisurecoder <leisurecoders@gmail.com>
2026-05-20 17:13:15 +0800

    Update nodePipeline.groovy

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -6,15 +6,30 @@ def call(Map config = [:]){
         agent any
         
         environment {
-            NODE_VERSION = config.version ?: '24'
+            NODE_VERSION = '25'
         }
 
         stages{
+
+            stage('Prepare'){
+                steps{
+                    script{
+                        def version = config.version ?; '25'
+                        env.NODE_VERSION = version
+                    }
+                }
+            }
 
             stage('Checkout'){
                 steps{
                     echo "checkout source from github..."
                     checkout scm
+                }
+            }
+
+            stage('Build'){
+                steps{
+                    echo "Using Node.js version: ${env.NODE_VERSION}"
                 }
             }
 
