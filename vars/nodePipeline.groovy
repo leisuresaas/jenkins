9068be12134e70f2919ac07df5372778f13ebff7
Leisurecoder <leisurecoders@gmail.com>
2026-05-20 19:03:19 +0800

    Update nodePipeline.groovy

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -66,7 +66,7 @@ def call(Map config = [:]){
                 steps{
                     sh '''
                         rm -rf /home/app/test/*
-                        sudo tar -xvf /home/archive/test.tar /home/app/test/
+                        tar -xvf /home/archive/test.tar /home/app/test/
                     '''
                 }
             }
